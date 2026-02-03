f3326a202dea9966073a3e2e437b3b9782faa8a1
Wasuremeno <wasuremono19@gmail.com>
2026-02-03 17:50:30 +0300

    feat: define cache interface contract

--- src/Cache/CacheInterface.php
+++ src/Cache/CacheInterface.php
@@ -3,7 +3,15 @@
 namespace Cache;
 
 interface CacheInterface{
+//Сохраняет значение в кэше
+    public function set(string $key, $value, ?int $ttl = null): bool;
+//Получает значение из кэша
+    public function get(string $key, $default = null);
+//Проверяет наличие ключа в кэше
+    public function has(string $key): bool;
+//Удаляет значение из кэша
+    public function delete(string $key): bool;
+//Очищает весь кэш
+    public function clear(): bool;
 
- public function set(string $key, $value, ?int $ttl = null): bool;
- 
 }
