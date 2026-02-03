0ef1d74cb849947318809b0725b9a89d3a7cdd9d
Wasuremeno <wasuremono19@gmail.com>
2026-02-03 18:03:01 +0300

    feat: implement abstract cache class

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -4,3 +4,14 @@ namespace Cache;
 
 use Cache\Exceptions\CacheException;
 
+abstract class Cache implements CacheInterface{
+    //Конфигурация
+    protected $config = [];
+    //Префикс для ключа 
+    protected $prefix = '';
+    //Конструктор
+    public function __construct(array $config){
+        $this->config = $config;
+        $this->prefix = $config['prefix'] ?? '';
+    }
+}
